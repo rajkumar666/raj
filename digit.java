078befd198cae8ed150e077d13fcb33a6e3bd186
rajkumar666 <rajkum3979@gmail.com>
2017-07-12 12:04:13 +0530

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,14 @@
+# raj
+import java.io.*;
+import java.util.*;
+public class dight
+{
+public static void main(String [] args)
+{
+int n,b;
+Scanner sc=new Scanner(System.in)
+n=sc.nextInt();
+b=Integer.toString(n).length();
+System.out.println(b);
+}
+}

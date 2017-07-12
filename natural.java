2a6d53813501af187c19a80743e95613dbfe1513
rajkumar666 <rajkum3979@gmail.com>
2017-07-12 12:36:14 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,19 @@
+# raj
+import java.io.*;
+import java.util.*;
+public class natural
+{
+
+public static void main(String[]args)
+{
+int n,i;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+whuile(i<=n)
+{
+sum +=i;
+i++;
+}
+System.out.println(i);
+}
+}
